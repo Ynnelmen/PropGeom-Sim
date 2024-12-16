a0ec9455830c37971324d1bdb93871523e646b7f
RhinerLenny <lenny.rhiner@gmx.ch>
2024-12-16 11:55:01 +0100

    Observer manager plotting and iso3745

--- Observer_Manager.py
+++ Observer_Manager.py
@@ -19,7 +19,7 @@ class ObserverManager:
         if radius is None:
             radius = default_radius
 
-        positions = [
+        iso_data = np.array([
             [0.336, -2.016, 0.462],
             [1.638, -1.260, 0.420],
             [1.638, 1.155, 0.651],
@@ -29,9 +29,9 @@ class ObserverManager:
             [-0.546, -1.365, 1.491],
             [1.554, -0.147, 1.407],
             [-0.546, 1.050, 1.743],
-            [0.210, -0.210, 2.079],
-        ] * (radius / default_radius)
-        return cls.from_positions(positions)
+            [0.210, -0.210, 2.079]
+        ]) * (radius / default_radius)
+        return cls.from_positions(iso_data)
 
     @classmethod
     def from_iso3745(cls, radius=None):
@@ -39,18 +39,21 @@ class ObserverManager:
         if radius is None:
             radius = default_radius
         iso_data = np.array([
-            [0, -1.887, 0, 0.844444444-0.75],
-            [1, 0.933111111, -1.616888889, 1.033333333-0.75],
-            [2, 0.914222222, 1.584777778, 1.222222222-0.75],
-            [3, -0.884, 1.531888889, 1.411111111-0.75],
-            [4, -0.844333333, -1.460111111, 1.6-0.75],
-            [5, 1.577222222, 0, 1.788888889-0.75],
-            [6, 0.717777778, 1.242888889, 1.977777778-0.75],
-            [7, -1.248555556, 0, 2.166666667-0.75],
-            [8, 0.496777778, -0.861333333, 2.355555556-0.75],
-            [9, 0.589333333, 0, 2.544444444-0.75]
+            [-1.887, 0, 0.844444444-0.75],
+            [0.933111111, -1.616888889, 1.033333333-0.75],
+            [0.914222222, 1.584777778, 1.222222222-0.75],
+            [-0.884, 1.531888889, 1.411111111-0.75],
+            [-0.844333333, -1.460111111, 1.6-0.75],
+            [1.577222222, 0, 1.788888889-0.75],
+            [0.717777778, 1.242888889, 1.977777778-0.75],
+            [-1.248555556, 0, 2.166666667-0.75],
+            [0.496777778, -0.861333333, 2.355555556-0.75],
+            [0.589333333, 0, 2.544444444-0.75]
         ]) * (radius / default_radius)
-        return cls.from_positions(iso_data[:, 1:])
+        # rotate 90 degrees around z-axis:
+        rotation_matrix = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+        iso_data = np.dot(iso_data, rotation_matrix)
+        return cls.from_positions(iso_data)
 
     @classmethod
     def from_fibonacci(cls, n_points, radius):
@@ -79,7 +82,7 @@ class ObserverManager:
         for num, observer in enumerate(self.observers):
             pos = observer()
             ax.scatter(pos[0], pos[1], pos[2], color='r', s=50)
-            ax.text(pos[0], pos[1], pos[2], '%d' % int(num), size=10, zorder=1)
+            ax.text(pos[0], pos[1], pos[2], '%d' % int(num+1), size=10, zorder=1)
 
     def plot_observer_positions(self):
         fig = plt.figure(figsize=(8, 8))
@@ -105,6 +108,14 @@ class ObserverManager:
 
         ax.plot_surface(x, y, z, color='c', alpha=0.3, rstride=5, cstride=5)
 
+        # plot the semi-circle in the y-z plane
+        u = np.linspace(0, np.pi, 100)
+        v = np.linspace(0, np.pi, 100)
+
+        ax.plot(np.zeros(100), np.cos(u)*radius, np.sin(v)*radius, color='blue', alpha=0.5, label="Blade Plane", linestyle='dashed')
+        ax.text(0, -radius, 0, 'Blade Plane', color='blue', fontsize=12)
+
+
         # Plot coordinate system axes with arrows
         ax.quiver(0, 0, 0, 1, 0, 0, color='r', arrow_length_ratio=0.1, label='X-axis')
         ax.quiver(0, 0, 0, 0, 1, 0, color='g', arrow_length_ratio=0.1, label='Y-axis')
@@ -117,7 +128,6 @@ class ObserverManager:
 
         plt.title('Receiver Positions')
         plt.show()
-
 
 if __name__ == "__main__":
     observers = ObserverManager.from_iso3745()
